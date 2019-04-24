d1358f783d551023071ad95d8b035365d719dc9c
Stéphane Ludwig <gitlab@stephane-ludwig.net>
2019-04-24 14:40:48 +0200

    Bump to Version 0.17.0.post2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="zeebe_grpc",
-    version="0.17.0.post1",
+    version="0.17.0.post2",
     author="Stéphane Ludwig",
     author_email="gitlab@stephane-ludwig.net",
     description="zeebe Python GRPC Gateway",
