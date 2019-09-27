fa92c362f036ff32214519ec9b6348db8786a4ab
gitlab@stephane-ludwig.net <sarahdlm15>
2019-09-27 10:32:05 +0200

    Added setuptools and wheel to requirements.txt

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="zeebe_grpc",
-    version="0.20.0.10",
+    version="0.20.0.11",
     author="Stéphane Ludwig",
     author_email="gitlab@stephane-ludwig.net",
     description="zeebe Python gRPC Gateway",
@@ -14,8 +14,8 @@ setuptools.setup(
     url="https://gitlab.com/stephane.ludwig/zeebe_python_grpc",
     packages=setuptools.find_packages(),
     install_requires=[
-        'grpcio==1.23.0',
-        'google==2.0.2',
+        'grpcio==1.24.0',
+        'protobuf==3.9.2',
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
