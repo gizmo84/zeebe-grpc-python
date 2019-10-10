d7f26b9dee47141e0b5285ceb6090f2652b25972
Stéphane Ludwig <gitlab@stephane-ludwig.net>
2019-10-10 11:19:05 +0200

    - Bump grpcio and grpcio-tools to 1.24.1 - Bump protobuf to 3.9.2 - Bump zeebe to 0.21.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="zeebe_grpc",
-    version="0.20.0.11",
+    version="0.21.1.0",
     author="Stéphane Ludwig",
     author_email="gitlab@stephane-ludwig.net",
     description="zeebe Python gRPC Gateway",
@@ -14,8 +14,8 @@ setuptools.setup(
     url="https://gitlab.com/stephane.ludwig/zeebe_python_grpc",
     packages=setuptools.find_packages(),
     install_requires=[
-        'grpcio==1.24.0',
-        'protobuf==3.9.2',
+        'grpcio==1.24.1',
+        'protobuf==3.10.0',
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
