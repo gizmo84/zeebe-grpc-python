acf665116e72d90f6b320a2f7a4b7a4ec51afc78
Stéphane Ludwig <gitlab@stephane-ludwig.net>
2020-05-05 16:44:57 +0200

    - Bump zeebe to 0.23.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="zeebe_grpc",
-    version="0.23.0.0",
+    version="0.23.1.0",
     author="Stéphane Ludwig",
     author_email="gitlab@stephane-ludwig.net",
     description="zeebe Python gRPC Gateway",
