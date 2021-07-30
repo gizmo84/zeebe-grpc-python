498124ef1af75a3288a48548f9d8177fc1098041
Stéphane Ludwig <gitlab@stephane-ludwig.net>
2021-07-30 12:05:47 +0000

    Update .gitlab-ci.yml, setup.py, requirements_dev.txt files

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="zeebe_grpc",
-    version="1.0.0",
+    version="1.1.1",
     author="Stéphane Ludwig",
     author_email="gitlab@stephane-ludwig.net",
     description="zeebe Python gRPC Gateway",
